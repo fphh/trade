31b173011edfcfe9e84080f001427fadab7c7d0a
Dr. Heinrich Hördegen <hoerdegen@funktional.info>
2018-07-08 18:35:29 +0200

    added comments to State

--- src/Trade/Type/State.hs
+++ src/Trade/Type/State.hs
@@ -5,9 +5,10 @@ module Trade.Type.State where
 import Trade.Report.Pretty
 
 
+-- | The trader's state.
 data State =
   Long
-  -- | Short
+  -- -- | Short
   | NoPosition
   deriving (Show, Eq, Ord)
 
