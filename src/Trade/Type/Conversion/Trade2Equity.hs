c531109c26a398bc271cde51d6b7c690a083f3dc
Dr. Heinrich Hördegen <hoerdegen@funktional.info>
2018-11-03 16:35:40 +0100

    fixed trade2equity

--- src/Trade/Type/Conversion/Trade2Equity.hs
+++ src/Trade/Type/Conversion/Trade2Equity.hs
@@ -16,22 +16,19 @@ import Trade.Help.SafeTail
 
 trade2equity :: (UnOHLC a) => (ohlc -> a) -> Equity -> TradeList t ohlc -> EquitySignal t
 trade2equity tradeAt (Equity eqty) (TradeList tl) =
-  let p (Trade NoPosition _) = False
-      p _ = True
-
-      g yys =
+  let g yys =
         let (tts, zs) = Vec.unzip yys
         in Vec.cons
            (shead "trade2equity" tts, 1)
            (Vec.zip (stail "trade2equity (1)" tts) (Vec.zipWith (/) (stail "trade2equity (2)" zs) zs))
 
-      unCl (Trade _ vs) = Vec.map (fmap (unOHLC . tradeAt)) vs
+      unCl (Trade NoPosition vs) = Vec.map (fmap (const 1.0)) vs
+      unCl (Trade Long vs) = Vec.map (fmap (unOHLC . tradeAt)) vs
       
-      cs = map (g . unCl) (filter p tl)
+      cs = map (g . unCl) tl
 
       (ts, ys) = Vec.unzip (Vec.concat cs)
 
       ysNew = Vec.map Equity (stail "trade2equity" (Vec.scanl (*) eqty ys))
       
   in Signal (Vec.zip ts ysNew)
-
